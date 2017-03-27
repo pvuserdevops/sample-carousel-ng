a4827ed6330330ad01335177ef92016d5340aec1
pvuserdevops <pvuserdevops@gmail.com>
2017-03-27 04:23:26 +0100

    path correction

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,1 @@
-import { Ngteste1Page } from './app.po';
 
-describe('ngteste1 App', function() {
-  let page: Ngteste1Page;
-
-  beforeEach(() => {
-    page = new Ngteste1Page();
-  });
-
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
-});
